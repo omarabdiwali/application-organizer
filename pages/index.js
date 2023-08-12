9a913152db5f16576a3674a22609a4f0cda2d72e
Omar Abdiwali <omarabdiwali17@gmail.com>
2023-08-12 16:26:49 -0600

    changed size of date

--- pages/index.js
+++ pages/index.js
@@ -222,7 +222,7 @@ export default function Home() {
                       <UpdateApplication func={update} ogTitle={title} ogUrl={url} ogStatus={status} button={<AiOutlineEdit />} />
                     </td>
                     <td className="border-b text-xs border-slate-300 dark:border-slate-700 p-3 text-slate-700 dark:text-slate-400">{title}</td>
-                    <td className="border-b text-sm border-slate-300 dark:border-slate-700 p-3 text-slate-700 dark:text-slate-400">{date}</td>
+                    <td className="border-b text-xs border-slate-300 dark:border-slate-700 p-3 text-slate-700 dark:text-slate-400">{date}</td>
                     <td className="border-b text-xs border-slate-300 dark:border-slate-700 p-3 text-slate-700 dark:text-slate-400"><a className="hover:underline" target="__blank" href={url}>{url}</a></td>
                     <td className="border-b border-slate-300 dark:border-slate-700 p-3 text-slate-700 dark:text-slate-400">{status.includes("Applied") ? <AiOutlineCheck className="m-auto" color="green" /> : <AiOutlineClose className="m-auto" color="red" />}</td>
                     <td className="border-b border-slate-300 dark:border-slate-700 p-3 text-slate-700 dark:text-slate-400">{status.includes("Online Assessment") ? <AiOutlineCheck className="m-auto" color="green" /> : <AiOutlineClose className="m-auto" color="red" />}</td>
