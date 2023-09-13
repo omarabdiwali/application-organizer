2a21aa3387f899f90e2dc8fd0278321c4efde76a
Omar Abdiwali <omarabdiwali17@gmail.com>
2023-09-13 01:45:19 -0600

    added style to button when disabled

--- components/deleteApplication.js
+++ components/deleteApplication.js
@@ -42,8 +42,8 @@ export default function DeleteApplication({ func, className, button, url }) {
                 Are you sure? You can&apos;t undo this action afterwards.
               </div>
               <div className="flex justify-end p-6 space-x-2 rounded-b border-gray-600">
-                <button disabled={disabled} className="text-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-red-600 disabled:bg-red-700 enabled:hover:bg-red-700 focus:ring-red-800" onClick={deleteItem}>Delete</button>
-                <button disabled={disabled} className="focus:ring-4 focus:outline-none rounded-lg border text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600" onClick={() => setOpen(false)}>Cancel</button>
+                <button disabled={disabled} className="disabled:opacity-75 text-black focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-red-600 disabled:bg-red-700 enabled:hover:bg-red-700 focus:ring-red-800" onClick={deleteItem}>Delete</button>
+                <button disabled={disabled} className="disabled:opacity-75 focus:ring-4 focus:outline-none rounded-lg border text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600" onClick={() => setOpen(false)}>Cancel</button>
               </div>
             </div>
           </div>
