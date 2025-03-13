b38d22af05d9e8ea9b64bf077b865d088a00baff
Omar Abdiwali <omarabdiwali17@gmail.com>
2025-03-12 23:12:53 -0600

    added enter key ability

--- components/addApplications.js
+++ components/addApplications.js
@@ -9,7 +9,8 @@ export default function AddApplication({ func, button, className }) {
   const [disabled, setDisabled] = useState(false);
   const [open, setOpen] = useState(false);
 
-  const addApplication = () => {
+  const addApplication = (e) => {
+    e.preventDefault();
     let trimTitle = title.trim();
     let trimUrl = url.trim();
 
@@ -74,26 +75,28 @@ export default function AddApplication({ func, button, className }) {
           <div className="relative m-auto w-full max-w-2xl max-h-full">
             <div className="relative rounded-lg shadow bg-slate-700">
               <div className="text-2xl font-bold p-3 m-3">Add Application</div>
-              <div className="flex space-x-5 flex-row m-5">
-                <div>Title:</div>
-                <input className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10 p-3" type="text" value={title} onChange={changeTitle}aria-label="Title" placeholder="Title..." />
-              </div>
-              <div className="flex m-5 space-x-5 flex-row m-5">
-                <div>URL:</div>
-                <input className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10 p-3" type="text" value={url} onChange={changeUrl} aria-label="URL" placeholder="URL..." />
-              </div>
-              <div className="flex m-5 space-y-2 flex-col m-5">
-                <div></div>
-                <label><input checked={status.includes("Applied")} onChange={e => changeStatus(e, "Applied")} className="mr-2" type="checkbox"></input>Applied</label>
-                <label><input checked={status.includes("Online")} onChange={e => changeStatus(e, "Online Assessment")} className="mr-2" type="checkbox"></input>Online Assessment</label>
-                <label><input checked={status.includes("Inter")} onChange={e => changeStatus(e, "Interviews")} className="mr-2" type="checkbox"></input>Interviews</label>
-                <label><input checked={status.includes("Offer")} onChange={e => changeStatus(e, "Offer")} className="mr-2" type="checkbox"></input>Offer</label>
-                <label><input checked={status.includes("Reject")} onChange={e => changeStatus(e, "Rejected")} className="mr-2" type="checkbox"></input>Rejected</label>
-              </div>
-              <div className="flex items-center justify-end p-6 space-x-2 rounded-b border-gray-600">
-                <button disabled={disabled} className="disabled:opacity-75 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-black text-center disabled:bg-blue-700 bg-blue-600 enabled:hover:bg-blue-700 focus:ring-blue-800" onClick={addApplication}>Add</button>
-                <button disabled={disabled} className="disabled:opacity-75 focus:ring-4 focus:outline-none rounded-lg border text-sm font-medium px-5 py-2.5 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600" onClick={closeModal}>Cancel</button>
-              </div>
+              <form onSubmit={addApplication}>
+                <div className="flex space-x-5 flex-row m-5">
+                  <div>Title:</div>
+                  <input className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10 p-3" type="text" value={title} onChange={changeTitle}aria-label="Title" placeholder="Title..." />
+                </div>
+                <div className="flex m-5 space-x-5 flex-row m-5">
+                  <div>URL:</div>
+                  <input className="focus:border-light-blue-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10 p-3" type="text" value={url} onChange={changeUrl} aria-label="URL" placeholder="URL..." />
+                </div>
+                <div className="flex m-5 space-y-2 flex-col m-5">
+                  <div></div>
+                  <label><input checked={status.includes("Applied")} onChange={e => changeStatus(e, "Applied")} className="mr-2" type="checkbox"></input>Applied</label>
+                  <label><input checked={status.includes("Online")} onChange={e => changeStatus(e, "Online Assessment")} className="mr-2" type="checkbox"></input>Online Assessment</label>
+                  <label><input checked={status.includes("Inter")} onChange={e => changeStatus(e, "Interviews")} className="mr-2" type="checkbox"></input>Interviews</label>
+                  <label><input checked={status.includes("Offer")} onChange={e => changeStatus(e, "Offer")} className="mr-2" type="checkbox"></input>Offer</label>
+                  <label><input checked={status.includes("Reject")} onChange={e => changeStatus(e, "Rejected")} className="mr-2" type="checkbox"></input>Rejected</label>
+                </div>
+                <div className="flex items-center justify-end p-6 space-x-2 rounded-b border-gray-600">
+                  <button disabled={disabled} className="disabled:opacity-75 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-black text-center disabled:bg-blue-700 bg-blue-600 enabled:hover:bg-blue-700 focus:ring-blue-800" onClick={addApplication}>Add</button>
+                  <button disabled={disabled} className="disabled:opacity-75 focus:ring-4 focus:outline-none rounded-lg border text-sm font-medium px-5 py-2.5 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600" onClick={closeModal}>Cancel</button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
